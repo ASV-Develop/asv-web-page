b177d20bf1fe1eb30779d285647c30c3acfd203a
Neorise456 <defneorise@gmail.com>
2026-03-05 12:53:06 +0900

    feat(home): add home page component and update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,19 @@
 import { Routes } from '@angular/router';
-import {AboutUs1} from './shared/pages/about-us-1/about-us-1';
-import {ContactUs1} from './shared/pages/contact-us-1/contact-us-1';
+import { HomePage } from './shared/pages/home/home';
+import { AboutUs1 } from './shared/pages/about-us-1/about-us-1';
+import { ContactUs1 } from './shared/pages/contact-us-1/contact-us-1';
 
 export const routes: Routes = [
   {
-    path:'', redirectTo: '/about-us', pathMatch: 'full'
+    path: '',
+    component: HomePage,
   },
   {
-    path:'about-us', component: AboutUs1
+    path: 'about-us',
+    component: AboutUs1,
   },
   {
-    path:'contact-us', component: ContactUs1
-  }
+    path: 'contact-us',
+    component: ContactUs1,
+  },
 ];
