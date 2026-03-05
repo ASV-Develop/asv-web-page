45e83386702cc5fbfeb76bd032a0cffdb6f85469
Pache <54517063+Pachieeee@users.noreply.github.com>
2026-03-05 11:28:32 -0500

    feat(routes): add company services route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {AboutUs1} from './shared/pages/about-us-1/about-us-1';
 import {ContactUs1} from './shared/pages/contact-us-1/contact-us-1';
+import { CompanyServices } from './shared/pages/company-services/company-services';
 
 export const routes: Routes = [
   {
@@ -11,5 +12,8 @@ export const routes: Routes = [
   },
   {
     path:'contact-us', component: ContactUs1
+  },
+  {
+    path:'services', component: CompanyServices
   }
 ];
